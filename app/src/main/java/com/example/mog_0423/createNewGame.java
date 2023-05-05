ccc6aa351cf12acf1cdbe5a0fc4eddb3a77d9746
Kera Linn <88988242+KeraLinn@users.noreply.github.com>
2023-05-05 16:20:47 -0500

    Added the components for screen_new_game.xml to createNewGame.java class, including methods to get the name, difficulty level, and avatar player has entered upon clicking the complete button.
    Still need to implement on click listeners for the checkboxes and avatars (when 1 is selected, if you select another it deselects the first and selects the new one).

--- app/src/main/java/com/example/mog_0423/createNewGame.java
+++ app/src/main/java/com/example/mog_0423/createNewGame.java
@@ -1,12 +1,76 @@
 package com.example.mog_0423;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class createNewGame extends AppCompatActivity {
+    String playerName;
+    int difficultyLevel;
+    int profileImage;
+
+    EditText player = findViewById(R.id.editTextTextPersonName2);
+    ImageButton avatar1 = findViewById(R.id.imageButton3);
+    ImageButton avatar2 = findViewById(R.id.imageButton4);
+    ImageButton avatar3 = findViewById(R.id.imageButton5);
+    ImageButton avatar4 = findViewById(R.id.imageButton6);
+    Button completeButton = findViewById(R.id.startButton);
+    CheckBox easyCheck = findViewById(R.id.easyCheck);
+    CheckBox mediumCheck = findViewById(R.id.mediumCheck);
+    CheckBox hardCheck = findViewById(R.id.hardcheck);
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_new_game);
+
+        completeButton.setOnClickListener((View v) -> {
+            playerName = getPlayerName();
+            difficultyLevel = getDifficultyLevel();
+            profileImage = getProfileImage();
+        });
     }
+
+
+
+    String getPlayerName() {
+        return player.toString();
+    }
+    int getDifficultyLevel(){
+        if (mediumCheck.isChecked()){
+            return 2;
+        }
+        else if (hardCheck.isChecked()){
+            return 3;
+        }
+        //if easy or none checked
+        else {
+            easyCheck.setChecked(true);
+            return 1;
+        }
+    }
+    int getProfileImage() {
+        if (avatar1.isSelected()){
+            return avatar1.getId();
+        }
+        else if (avatar2.isSelected()){
+            return avatar2.getId();
+        }
+        else if (avatar3.isSelected()){
+            return avatar3.getId();
+        }
+        else if (avatar4.isSelected()){
+            return avatar4.getId();
+        }
+        return 0;
+    }
+
+
+
+
+
 }
